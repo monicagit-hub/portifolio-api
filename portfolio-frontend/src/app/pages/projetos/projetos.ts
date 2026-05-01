4d6921810b676fc40082c727ac2e87846da1635f
monicagit-hub <githubmonica@gmail.com>
2026-05-01 15:01:39 -0300

    feat: página de projetos conectada com a API

--- portfolio-frontend/src/app/pages/projetos/projetos.ts
+++ portfolio-frontend/src/app/pages/projetos/projetos.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProjetoService, Projeto } from '../../services/projeto';
 
 @Component({
   selector: 'app-projetos',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './projetos.html',
-  styleUrl: './projetos.css',
+  styleUrl: './projetos.css'
 })
-export class Projetos {}
+export class Projetos implements OnInit {
+
+  projetos: Projeto[] = [];
+
+   constructor(private projetoService: ProjetoService) {}
+
+  ngOnInit(): void {
+    this.projetoService.listarTodos().subscribe(dados => {
+      this.projetos = dados;
+    });
+  }
+}
